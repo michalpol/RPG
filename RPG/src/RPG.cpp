b8a1150e2ea2ac185a3935d91e93b789d126a93d
michalpol <gry@michalpol.pl>
2012-02-06 17:18:17 +0100

    Wstępny commit z całą konfiguracją

--- RPG/src/RPG.cpp
+++ RPG/src/RPG.cpp
@@ -1,15 +1,21 @@
 //============================================================================
 // Name        : RPG.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : Michalpol
+// Version     : 0 (Kolejny numer edycji)
+// Copyright   : -
+// Description : G³ówna aplikacja gry - w¹tek steruj¹cy wywietlaniem
 //============================================================================
 
+//INCLUDES HERE
 #include <iostream>
+#include <cstdlib>
+
+//NAMESPACES HERE
 using namespace std;
 
+//VARIABLES HERE
+
+//FUNCTIONS HERE
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+//Dummy main function
 }
